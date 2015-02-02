fbb8c92cbc59d6d5ab1cd53430321320d91b7415
daiyongzhi <daiyongzhi@foxmail.com>
2015-02-02 09:37:44 +0800

    nothing

--- src/cn/dyz/main/Main.java
+++ src/cn/dyz/main/Main.java
@@ -6,10 +6,11 @@ import java.net.SocketAddress;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 
 import cn.dyz.conservation.DisconnectConversation;
+import cn.dyz.conservation.LoginConversation;
 import cn.dyz.conservation.OpenAppConversation;
-import cn.dyz.conservation.SignUpConversation;
 import cn.dyz.conservation.base.ConversationChain;
 import cn.dyz.msg.request.DisconnectRequest1000;
+import cn.dyz.msg.request.LoginRequest1003;
 import cn.dyz.msg.request.OpenAppRequest1001;
 import cn.dyz.msg.request.SignUpRequest1005;
 
@@ -43,8 +44,8 @@ public class Main{
 	
 	public static void buildConversation(ConversationChain chain){
 		chain.addConversation(new OpenAppConversation(0, new OpenAppRequest1001()));
-		chain.addConversation(new SignUpConversation(10, new SignUpRequest1005("liudehua", "12306", "dyz@foxmail.com", "110")));
-		
+		//chain.addConversation(new SignUpConversation(10, new SignUpRequest1005("刘德华", "110", "dyz@foxmail.com", "1234")));
+		chain.addConversation(new LoginConversation(0, new LoginRequest1003("110", "1234")));
 		
 		chain.addConversation(new DisconnectConversation(0, new DisconnectRequest1000()));//断开链接请求
 	}
